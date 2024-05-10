288c6ef21465d2254e361517ea57300f476b0354
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-10 20:28:53 +0530

    Added shadcn ui, added header component.

---
+++ app/_components/Header.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='p-5 flex justify-between items-center border shadow-sm'>
+            <Image
+            src={'./logo.svg'}
+            alt='Logo'
+            width={100}
+            height={100}
+             />
+             <Button>Get Started</Button>
+        </div>
+    )
+}
+
+export default Header;
