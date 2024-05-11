25200c0b97be671682db9fc15bc2a6e171287faf
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-11 13:40:37 +0530

    Added userprofile button

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,17 +1,20 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { UserButton, useUser } from '@clerk/clerk-react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Header = () => {
+    const { user, isSignedIn } = useUser();
     return (
         <div className='p-5 flex justify-between items-center border shadow-sm'>
             <Image
-            src={'./logo.svg'}
-            alt='Logo'
-            width={100}
-            height={100}
-             />
-            <Button><Link href={'/sign-in'}>Get Started</Link></Button>
+                src={'./logo.svg'}
+                alt='Logo'
+                width={100}
+                height={100} />
+            {isSignedIn ? <UserButton /> : <Button><Link href={'/sign-in'}>Get Started</Link></Button>}
         </div>
     )
 }
