879afe41a1da4e64dc81bdf639553a8f4176255c
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-11 17:08:10 +0530

    Added sidenav on active styles.

--- app/(routes)/dashboard/_components/sidenav.tsx
+++ app/(routes)/dashboard/_components/sidenav.tsx
@@ -1,28 +1,36 @@
+'use client';
+
 import { UserButton } from "@clerk/nextjs";
 import { Info, LayoutGrid, PiggyBank, ReceiptText, ShieldCheck } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidenav = () => {
     const Menu = [
-        { id: 1, name: 'Dashboard', icon: LayoutGrid },
-        { id: 2, name: 'Budget', icon: PiggyBank },
-        { id: 3, name: 'Expenses', icon: ReceiptText },
-        { id: 4, name: 'Upgrade', icon: ShieldCheck },
-        { id: 5, name: 'About', icon: Info },
+        { id: 1, name: 'Dashboard', icon: LayoutGrid, path: '/dashboard' },
+        { id: 2, name: 'Budget', icon: PiggyBank, path: 'dashboard/budget' },
+        { id: 3, name: 'Expenses', icon: ReceiptText, path: 'dashboard/expenses' },
+        { id: 4, name: 'Upgrade', icon: ShieldCheck, path: 'dashboard/upgrade' },
+        { id: 5, name: 'About', icon: Info, path: 'dashboard/about' },
     ]
+
+    const pathName = usePathname();
     return (
         <div className="h-screen p-5 border shadow-md">
             <Image src={'./logo.svg'} alt="logo" width={80} height={60} />
             <div className="mt-5">
                 {Menu.map((item, index) => (
-                    <span className="flex gap-2 p-5 items-center text-gray-600 font-medium rounded-md cursor-pointer hover:text-primary hover:bg-blue-200" key={item.id}><item.icon />{item.name}</span>
+                    <Link href={item.path} key={item.id}>
+                        <span className={`flex gap-2 p-5 mb-2 items-center text-gray-600 font-medium rounded-md cursor-pointer hover:text-primary hover:bg-blue-100 ${pathName === item.path && 'text-primary bg-blue-100'}`}><item.icon />{item.name}</span>
+                    </Link>
                 ))}
             </div>
             <div className="fixed p-5 flex gap-2 bottom-10 items-center">
                 <span>Profile</span>
                 <UserButton />
             </div>
-        </div>
+        </div >
     )
 }
 export default Sidenav;
