eb4671e533eee77b527289f096cb41fe5da95498
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-24 18:58:11 +0530

    Added previous button

--- app/(routes)/dashboard/budget/page.tsx
+++ app/(routes)/dashboard/budget/page.tsx
@@ -1,9 +1,24 @@
+'use client';
+
 import React from 'react'
 import BudgetList from './_components/Bu/BudgetList';
+import { ArrowLeftCircle } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
 
 const Budgets = () => {
+
+  const router = useRouter();
+
+  const goToPrevious = () => {
+    router.back();
+  }
+
   return (
     <div className='p-10'>
+      <Button onClick={goToPrevious} variant={'ghost'} size={'icon'} className='float-left mr-4'>
+        <ArrowLeftCircle height={32} width={32} />
+      </Button>
       <h2 className='font-bold text-3xl'>My Budgets</h2>
       <BudgetList />
     </div>
