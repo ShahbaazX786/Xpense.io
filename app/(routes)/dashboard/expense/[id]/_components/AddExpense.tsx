49ba08e792526a909db822e4fdc86895e10c2f3b
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-24 18:57:58 +0530

    Added loading animation and field reset on form submit.

--- app/(routes)/dashboard/expense/[id]/_components/AddExpense.tsx
+++ app/(routes)/dashboard/expense/[id]/_components/AddExpense.tsx
@@ -5,14 +5,17 @@ import { Budgets, Expenses } from "@/utils/schema"
 import { useState } from "react"
 import { toast } from "sonner"
 import moment from "moment";
+import { LucideLoader2 } from "lucide-react"
 
 const AddExpense = ({ paramId, user, refreshData }: any) => {
 
     const [name, setName] = useState('Name')
     const [amount, setAmount] = useState(0)
+    const [loading, setLoading] = useState(false);
 
     const addNewExpense = async () => {
         try {
+            setLoading(true);
             const result = await db.insert(Expenses).values({
                 name: name,
                 amount: amount,
@@ -22,9 +25,13 @@ const AddExpense = ({ paramId, user, refreshData }: any) => {
 
             if (result) {
                 refreshData();
+                setAmount(0);
+                setName('');
+                setLoading(false);
                 toast.success('Expense Added Successfully');
             }
         } catch (error) {
+            setLoading(false);
             toast.error('Error while Adding New Expense');
         }
 
@@ -35,13 +42,13 @@ const AddExpense = ({ paramId, user, refreshData }: any) => {
             <h2 className="font-bold text-lg">Add Expense</h2>
             <div className='mt-2'>
                 <p className='text-black font-medium my-1'>Expense Name:</p>
-                <Input placeholder='e.g. Furniture' onChange={(e) => { setName(e.target.value) }} />
+                <Input placeholder='e.g. Furniture' value={name} onChange={(e) => { setName(e.target.value) }} />
             </div>
             <div className='mt-2'>
                 <p className='text-black font-medium my-1'>Expense Amount:</p>
-                <Input placeholder='e.g. 5000$' onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
+                <Input placeholder='e.g. 5000$' value={amount} onChange={(e) => { setAmount(parseInt(e.target.value)) }} />
             </div>
-            <Button disabled={!(name && amount)} className="mt-3 w-full" onClick={addNewExpense}>Add New Expense</Button>
+            <Button disabled={!(name && amount)} className="mt-3 w-full" onClick={addNewExpense}>{loading ? <LucideLoader2 className="animate-spin"/> : 'Add New Expense'}</Button>
         </div>
     )
 }
