c5aaeea7f21b43ba9faa8b4a03db2f1f50715fc0
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-25 12:21:13 +0530

    Added Footer Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
+import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <main className="">
+    <main>
       {/* Bismillah */}
       <Header />
-      <Hero />
+        <Hero />
+      <Footer />
     </main>
   );
 }
