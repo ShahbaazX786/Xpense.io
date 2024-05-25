fd17160fe010354a504bd1571302503ac1d5fcf4
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-25 12:25:47 +0530

    Updated button text

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
                     <p className="text-3xl font-extrabold sm:text-6xl">Manage Your Expense Easily<strong className="font-extrabold text-primary sm:block mt-2"> Full Insights Of Your Money.</strong></p>
                     <p className="mt-4 sm:text-xl relaxed">Start Creating your budget and start saving money.</p>
                     <div className="mt-8 flex flex-wrap justify-center gap-4">
-                        <Link className="block w-full rounded bg-primary px-12 py-5 text-md font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-800 sm:w-auto" href="/sign-up"> Get Started Now! </Link>
+                        <Link className="block w-full rounded bg-primary px-12 py-5 text-md font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-800 sm:w-auto" href="/sign-up"> Create an Account Now! </Link>
                     </div>
                 </div>
             </div>
