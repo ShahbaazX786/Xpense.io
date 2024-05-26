cf201a6126b5c62f0fbd64581956fe4173102adf
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-26 07:49:01 +0530

    Added theme switcher in dashboard with updated search bar.

--- app/(routes)/dashboard/_components/dashboardHeader.tsx
+++ app/(routes)/dashboard/_components/dashboardHeader.tsx
@@ -1,10 +1,20 @@
+import { DarkModeToggleButton } from "@/app/_components/DarkModeToggleButton";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { UserButton } from "@clerk/nextjs";
+import { Search } from "lucide-react";
 
 const DashboardHeader = () => {
     return (
-        <div className="border-b shadow-sm p-5 flex justify-between">
-            <div>Search</div>
-            <div>
+        <div className="border-b shadow-sm p-5 flex justify-between gap-2">
+            <div className="flex justify-center items-center">
+                <Input placeholder="Search..." className="min-w-[60vw]" />
+                <Button variant={'ghost'} size={'icon'} className="border">
+                    <Search />
+                </Button>
+            </div>
+            <div className="flex justify-center items-center gap-3">
+                <DarkModeToggleButton />
                 <UserButton />
             </div>
         </div>
