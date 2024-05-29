37e12d6573c35de5299da0f950a82429de4b33d5
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-29 18:40:04 +0530

    updated nextconfig to use temp urls

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,11 @@ const nextConfig = {
                 hostname: 'i.pravatar.cc',
                 pathname: '**'
             },
+            {
+                protocol: 'https',
+                hostname: 'i.ibb.co',
+                pathname: '**'
+            },
         ]
     }
 };
