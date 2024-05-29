7b63592d76f1a79ce54f1ca6dc0b8c8900d77fbf
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-29 18:18:12 +0530

    Refactoring

---
+++ app/dashboard/_components/UserProfileCard.tsx
@@ -0,0 +1,21 @@
+import { UserButton, useUser } from '@clerk/nextjs';
+
+const UserProfileCard = () => {
+    const { user } = useUser();
+
+    return (
+        <div className='drop-shadow-lg flex flex-col justify-start items-start p-5'>
+            <div className="flex gap-2 items-center justify-start w-full">
+                <UserButton />
+                <div className='flex flex-col justify-center items-center'>
+                    <span className='font-semibold text-base text-gray-600'>
+                        {user?.fullName}
+                    </span>
+                    <span className='text-sm font-bold ring-1 ring-purple-400'>Base Plan</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserProfileCard;
