d875db66908ef9e4cd22b34f844382520f08d437
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-29 17:23:37 +0530

    Updated next config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com', 'i.pravatar.cc']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                pathname: '**'
+            },
+            {
+                protocol: 'https',
+                hostname: 'i.pravatar.cc',
+                pathname: '**'
+            },
+        ]
     }
 };
 
