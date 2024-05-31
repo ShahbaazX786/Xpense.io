417a83bcf533a8c9440c685beb50fa4187ed2c43
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-31 20:05:05 +0530

    Simplified dashboard header

--- app/dashboard/_components/DashboardHeader.tsx
+++ app/dashboard/_components/DashboardHeader.tsx
@@ -1,15 +1,9 @@
 import { DarkModeToggleButton } from "@/app/_components/DarkModeToggleButton";
-import { Input } from "@/components/ui/input";
 
 const DashboardHeader = () => {
     return (
-        <div className="border-b shadow-sm p-5 flex justify-between gap-2">
-            <div className="flex justify-center items-center">
-                <Input placeholder="Search..." className="min-w-[60vw]" />
-            </div>
-            <div className="flex justify-center items-center gap-3">
-                <DarkModeToggleButton />
-            </div>
+        <div className="border-b shadow-sm p-5 flex justify-end gap-2">
+            <DarkModeToggleButton />
         </div>
     )
 }
