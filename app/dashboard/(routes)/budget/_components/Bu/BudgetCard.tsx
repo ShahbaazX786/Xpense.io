d1ec6c8ee514a25dfa8b6a780f802678aed1add0
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-31 19:27:58 +0530

    Added prop types, added proper styling for progress

--- app/dashboard/(routes)/budget/_components/Bu/BudgetCard.tsx
+++ app/dashboard/(routes)/budget/_components/Bu/BudgetCard.tsx
@@ -1,16 +1,17 @@
+import { BudgetCardProps } from "@/lib/types";
 import Link from "next/link";
 
-const BudgetCard = (budget: any) => {
+const BudgetCard = ({ budget }: { budget: BudgetCardProps }) => {
 
     const calculateProgress = () => {
-        const percentage = (budget.totalSpend / budget.amount) * 100;
+        const percentage = (budget?.totalSpend! / parseInt(budget?.amount!)) * 100;
         return percentage.toFixed(2);
     }
 
     return (
-        <Link href={`/dashboard/expenses/${budget?.id}`}>
-            <div className="p-5 border rounded-lg hover:shadow-lg cursor-pointer">
-                <div className="flex gap-2 items-center justify-center">
+        <Link href={`/dashboard/expense/${budget?.id}`}>
+            <div className="p-4 border rounded-lg hover:shadow-lg cursor-pointer">
+                <div className="flex gap-2 items-center justify-between">
                     <div className="flex gap-2 items-center">
                         <h2 className="text-2xl bg-slate-100 rounded-full px-4 p-3">{budget.icon}</h2>
                         <div>
@@ -23,10 +24,10 @@ const BudgetCard = (budget: any) => {
                 <div className="mt-5">
                     <div className="flex items-center justify-between mb-4">
                         <p className="text-base text-slate-800">Spent: ${budget.totalSpend ? budget.totalSpend : 0}</p>
-                        <p className="text-base text-slate-800">Remaining: ${budget.amount - budget.totalSpend}</p>
+                        <p className="text-base text-slate-800">Remaining: ${parseInt(budget.amount) - budget.totalSpend}</p>
                     </div>
-                    <div className="w-full bg-slate-300 rounded-full">
-                        <div className=" bg-primary rounded-full" style={{ width: `${calculateProgress()}%` }}></div>
+                    <div className="w-full bg-slate-300 rounded-full h-2">
+                        <div className=" bg-primary rounded-full h-2" style={{ width: `${calculateProgress()}%` }}></div>
                     </div>
                 </div>
             </div>
