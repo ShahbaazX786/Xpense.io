f04ce76cb9d08b13bfa472c6fa88138d306e754f
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-05-31 20:03:07 +0530

    Removed search button

--- app/dashboard/_components/DashboardHeader.tsx
+++ app/dashboard/_components/DashboardHeader.tsx
@@ -1,16 +1,11 @@
 import { DarkModeToggleButton } from "@/app/_components/DarkModeToggleButton";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Search } from "lucide-react";
 
 const DashboardHeader = () => {
     return (
         <div className="border-b shadow-sm p-5 flex justify-between gap-2">
             <div className="flex justify-center items-center">
                 <Input placeholder="Search..." className="min-w-[60vw]" />
-                <Button variant={'ghost'} size={'icon'} className="border">
-                    <Search />
-                </Button>
             </div>
             <div className="flex justify-center items-center gap-3">
                 <DarkModeToggleButton />
